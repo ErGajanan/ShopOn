38b453f31d7d30e89b39e55aabe410e5b051fa8b
Gajanan <gajanankumbhar89@gmail.com>
2024-03-15 20:45:46 +0530

    "added semicolor"

---
+++ src/main/java/gajanan/ShopOn/entity/Booking.java
@@ -0,0 +1,14 @@
+package gajanan.ShopOn.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Booking
+{
+    @Id
+    private int id
+    private String pickUpLocation;
+}
